6f4223db28b8cb3c343818d223580099c5f5cd6d
Baranger <pbaranger10@gmail.com>
2015-01-08 23:41:05 +0100

    Code de ConfigurationJour

--- Android/app/src/main/java/com/l2gb/applipfe/ConfigurationJour.java
+++ Android/app/src/main/java/com/l2gb/applipfe/ConfigurationJour.java
@@ -1,20 +1,66 @@
 package com.l2gb.applipfe;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TableRow.LayoutParams;
+import android.widget.TextView;
 
 
 public class ConfigurationJour extends ActionBarActivity {
+
+    private int crenauIndice=0;
+    private LayoutParams layoutParams;
+    private TableRow tr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration_jour);
+
+
+
+        this.layoutParams= new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
+        this.layoutParams.setMargins(2, 2, 2, 2);
+
+
+        /*for(int i=0; i<crenauIndice; i++) {
+
+        }*/
     }
 
+    /** Called when the user clicks the Send button */
+    public void addCreneau(View view) {
+        // Do something in response to button
+        crenauIndice++;
+        TableLayout tl = (TableLayout) findViewById(R.id.tablelayoutId);
+        tr = new TableRow(this);
+        tr.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        tr.addView(generateTextView("test", layoutParams));
+        tr.addView(generateTextView(String.valueOf(crenauIndice), layoutParams));
+        tl.addView(tr, layoutParams);
+    }
 
+    public TextView generateTextView(String texte, LayoutParams ly) {
+        TextView result = new TextView(this);
+        result.setBackgroundColor(Color.LTGRAY);
+        result.setTextColor(Color.DKGRAY);
+        result.setGravity(Gravity.CENTER);
+        result.setPadding(2, 2, 2, 2);
+        result.setText(texte);
+        result.setTextSize(20);
+        result.setVisibility(View.VISIBLE);
+        result.setLayoutParams(ly);
+        return result;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
